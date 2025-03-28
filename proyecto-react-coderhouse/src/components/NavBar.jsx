1d1a674f404a950f31702feaf5a9250cf69649fe
Francisco C <ingefran57@gmail.com>
2025-03-28 13:34:16 -0300

    remplazo BarNav.css

--- proyecto-react-coderhouse/src/components/NavBar.jsx
+++ proyecto-react-coderhouse/src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import './BotonCategorico.css'
 import './CartWidget.css'
-import './NavBar.css'
+import './BarNav.css'
 import ButtonCategory from './ButtonCategory.jsx'
 import CartWidget from './CartWidget.jsx'
 import { Link } from 'react-router-dom'
